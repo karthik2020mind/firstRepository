32c1d080cc2f7831c52afdf06ddf55eb2710d009
karthik2020mind <mkarthikraja111@gmail.com>
2021-05-11 23:16:13 +0530

    committed

---
+++ gitProject/src/gitProject/firstpackage/Addition.java
@@ -0,0 +1,13 @@
+package gitProject.firstpackage;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		int b=8;
+		
+		System.out.println(a+b);
+	}
+
+}
---
+++ gitProject/src/gitProject/firstpackage/secondproject/Division.java
@@ -0,0 +1,9 @@
+package gitProject.firstpackage.secondproject;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int a=9,b=3;
+		System.out.println(a/b);
+	}
+}
